d8079cdfd3b60ad85aea5f4e4a551b3c8a652e9f
waseemR02 <waseem.riaz.9999@gmail.com>
2023-12-31 15:32:51 +0530

    Add macros for can messages

---
+++ kcan.h
@@ -0,0 +1,18 @@
+/*  
+ * Contains macros for kyvernitis CAN.
+ */
+
+#define BIO_ARM_ID 0x300
+#define ARM_ID 0x200
+
+/*
+* Actuators will range from 10 - 25 
+* Roboclaws- 2 
+* Sabertooths 3
+* Servos will range from 15 - 24
+*/
+#define ACTUATOR_COMMAND_ID 10
+
+#define ROBOCLAW_BASE_ID 10
+#define SABERTOOTH_BASE_ID 12
+#define SERVO_BASE_ID 15
